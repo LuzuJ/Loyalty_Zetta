7339cb5783127e453692e9ef5d6f4ac38ceca7e2
Jonathan Luzuriaga <118029879+LuzuJ@users.noreply.github.com>
2024-08-26 19:08:49 -0500

    Delete public directory

--- public/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleError = void 0;
-const handleError = (res, statusCode, message) => {
-    res.statusCode = statusCode;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ status: statusCode, message }));
-};
-exports.handleError = handleError;
